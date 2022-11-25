d11bd292c017e389ab723f5ff6ebd628b645cc26
priyanshr1972 <priyanshr1972@gmail.com>
2022-11-25 22:56:05 +0530

    webpj1

--- script.js
+++ script.js
@@ -1,15 +1,35 @@
-the = document.querySelectorAll('[name="theme"]');
-// console.log(th);
-the.forEach((theme) => {
-  theme.addEventListener("click", () => localStorage.setItem("theme", theme.id));
+var search = document.getElementById('sear');
+var form = document.getElementById('form');
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  search_movie();
 });
-
-function app() {
-  st = localStorage.getItem("theme"); 
-  the.forEach((theme) => {
-    if (theme.id === st) {
-      theme.checked = true;
-    }
-  });
+function search_movie() {
+    var url_2 = `https://api.tvmaze.com/search/shows?q=${search.value}`;
+    // console.log(search.value);
+    fetch(url_2)
+        .then((res) => {
+            // console.log(res.json());
+            return res.json();
+        })
+        .then((data) => {
+            console.log(data);
+            add_search(data);
+        });
 }
-document.onload = app();
+function add_search(data) {
+      var search_movies = document.querySelector(".s-box"); 
+      var movies = document.querySelector(".moviess");
+      movies.innerHTML='';
+      console.log(data);
+      for (const movie in data) {
+          const imgg = data[movie].show.image.original;
+          // console.log(imgg);
+          movies.innerHTML += `<div class='movie'>
+                      <img src=${imgg} height="200"/>  
+                  </div>`;
+      };
+      search_movies.style.visibility = "visible";
+    
+  };
+// search_movie();
